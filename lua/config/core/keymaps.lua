58ba35defbe35ebbe20258d9a4271146e3c85c0b
wyatt-avilla <wyattmurphy1@gmail.com>
2024-02-07 23:28:15 -0800

    keybinds again?

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -1,15 +1,15 @@
 vim.g.mapleader = " "
 
 -- colemak dh movement rebinds
-vim.keymap.set({'n', 'x'}, 'n', 'h', { noremap = true })
-vim.keymap.set({'n', 'x'}, 'e', 'j', { noremap = true })
-vim.keymap.set({'n', 'x'}, 'i', 'k', { noremap = true })
-vim.keymap.set({'n', 'x'}, 'o', 'l', { noremap = true })
+vim.keymap.set({ "n", "x" }, "n", "h", { noremap = true })
+vim.keymap.set({ "n", "x" }, "e", "j", { noremap = true })
+vim.keymap.set({ "n", "x" }, "i", "k", { noremap = true })
+vim.keymap.set({ "n", "x" }, "o", "l", { noremap = true })
 
-vim.keymap.set({'n', 'v'}, 'h', 'i', { noremap = true })
-vim.keymap.set({'n', 'v'}, 'j', 'o', { noremap = true })
-vim.keymap.set({'n', 'v'}, 'k', 'n', { noremap = true })
-vim.keymap.set({'n', 'v'}, 'l', 'e', { noremap = true })
+vim.keymap.set({ "n", "v" }, "h", "i", { noremap = true })
+vim.keymap.set({ "n", "v" }, "j", "o", { noremap = true })
+vim.keymap.set({ "n", "v" }, "k", "n", { noremap = true })
+vim.keymap.set({ "n", "v" }, "l", "e", { noremap = true })
 
 -- :wq as root
 vim.cmd([[command! -nargs=0 WW :execute 'w !doas tee % >/dev/null' | q!]])
@@ -20,4 +20,4 @@ vim.api.nvim_set_keymap("i", "<kEnter>", "<Enter>", {})
 vim.api.nvim_set_keymap("c", "<kEnter>", "<Enter>", {})
 
 -- delete highlighted without yanking
-vim.api.nvim_set_keymap('x', 'x', '\"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "x", '"_d', { noremap = true, silent = true })
