3fd6a950e6ef22522c9317240cfbf13e68ae2d81
wyatt-avilla <wyattmurphy1@gmail.com>
2024-02-07 22:17:53 -0800

    rust moment

---
+++ lua/config/plugins/rustaceanvim.lua
@@ -0,0 +1,11 @@
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^4', -- Recommended
+  init = function()
+    vim.g.rustaceanvim = {
+        hover_actions = {
+            auto_focus = true,
+        }
+    }
+  end
+}
