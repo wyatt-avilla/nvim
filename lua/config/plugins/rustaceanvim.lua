fd6d9db0a4ed7c5748f439d27379da6737466a0b
wyatt-avilla <wyattmurphy1@gmail.com>
2024-02-08 18:40:20 -0800

    cleaner rust setup

--- lua/config/plugins/rustaceanvim.lua
+++ lua/config/plugins/rustaceanvim.lua
@@ -1,11 +1,5 @@
 return {
   "mrcjkb/rustaceanvim",
   version = "^4", -- Recommended
-  init = function()
-    vim.g.rustaceanvim = {
-      hover_actions = {
-        auto_focus = true,
-      },
-    }
-  end,
+  ft = { "rust" },
 }
