e2a179360ae999ce86091bdf28f9f2198442ce1b
wyatt-avilla <wyattmurphy1@gmail.com>
2024-06-12 07:48:59 -0700

    -codio

--- lua/config/core/autocmds.lua
+++ lua/config/core/autocmds.lua
@@ -7,28 +7,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = highlight_group,
   pattern = "*",
 })
-
-local codio_group = vim.api.nvim_create_augroup("CodioGroup", { clear = true })
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  callback = function()
-    vim.bo.filetype = "cpp"
-    vim.b.disable_autoformat = true
-  end,
-  group = codio_group,
-  pattern = "codio.com_*.txt",
-})
-
-vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-  callback = function(e)
-    if vim.g.timer_started == true then
-      return
-    end
-    vim.g.timer_started = true
-    vim.fn.timer_start(1500, function()
-      vim.g.timer_started = false
-      vim.cmd("silent! w")
-    end)
-  end,
-  group = codio_group,
-  pattern = "codio.com_*.txt",
-})
--- lua/config/plugins/firenvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "glacambre/firenvim",
-  enabled = true,
-  lazy = not vim.g.started_by_firenvim,
-  build = function()
-    vim.fn["firenvim#install"](0)
-  end,
-}
